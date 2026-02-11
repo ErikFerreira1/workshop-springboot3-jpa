ead3d48c80b16b2b8607b93ec1084d0e51cab83f
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2026-02-10 21:15:07 -0300

    Order, Instant, ISO 8601

--- src/main/java/org/educandoweb/course/resources/OrderResource.java
+++ src/main/java/org/educandoweb/course/resources/OrderResource.java
@@ -1,7 +1,7 @@
 package org.educandoweb.course.resources;
 
-import org.educandoweb.course.entities.User;
-import org.educandoweb.course.services.UserService;
+import org.educandoweb.course.entities.Order;
+import org.educandoweb.course.services.OrderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,24 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/orders")
+public class OrderResource {
 
-    private final UserService service;
+    private final OrderService service;
 
-    public UserResource(UserService service) {
+    public OrderResource(OrderService service) {
         this.service = service;
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        List<User> list = service.findAll();
+    public ResponseEntity<List<Order>> findAll() {
+        List<Order> list = service.findAll();
         return ResponseEntity.ok().body(list);
 
     }
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id) {
-        User obj = service.findById(id);
+    public ResponseEntity<Order> findById(@PathVariable Long id) {
+        Order obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/org/educandoweb/course/services/OrderService.java
@@ -0,0 +1,29 @@
+package org.educandoweb.course.services;
+
+import org.educandoweb.course.entities.Order;
+import org.educandoweb.course.repositories.OrderRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository repository;
+
+    public OrderService(OrderRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+
+    }
+}
