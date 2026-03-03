e2e42787f544a7506486483c48a24a0a25782307
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2026-03-03 15:40:57 -0300

    Exception handling - findById

--- src/main/java/org/educandoweb/course/services/UserService.java
+++ src/main/java/org/educandoweb/course/services/UserService.java
@@ -3,6 +3,7 @@ package org.educandoweb.course.services;
 import org.educandoweb.course.entities.User;
 import org.educandoweb.course.repositories.UserRepository;
 
+import org.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,7 +24,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/org/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package org.educandoweb.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+
+
+}
