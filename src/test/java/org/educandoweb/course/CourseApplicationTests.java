0e1753fdd0e3984301c040c72aadf30544c9fe1b
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2026-03-07 21:46:02 -0300

    Deploy render

--- src/test/java/org/educandoweb/course/CourseApplicationTests.java
+++ src/test/java/org/educandoweb/course/CourseApplicationTests.java
@@ -1,9 +1,11 @@
 package org.educandoweb.course;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CourseApplicationTests {
 
     @Test
